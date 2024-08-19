1302ead88b2b41f2b0606f998ea492df7d64df10
Hascihan26 <hasancihanbilgin@gmail.com>
2024-08-19 12:20:19 +0300

    Initial Commit

---
+++ Radio/RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioApp.swift
+//  Radio
+//
+//  Created by Hasan Cihan Bilgin on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
